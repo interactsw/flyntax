fe38e154148bbc0f0ba21132be540c814dc026b6
Ian Griffiths <ian@interact-sw.co.uk>
2016-07-11 10:17:39 +0100

    Add build version scripting
    Added a Powershell script that determines the version from the TeamCity
    build counter, and ensures that this is set consistently across
    assemblies, nuspec, vsixmanifest files, and the TeamCity buildNumber.

---
+++ scripts/build/SetBuildVersion.ps1
@@ -0,0 +1,41 @@
+﻿Param(
+  [string]$BuildCounter,
+  [string]$AssemblyInfoPath
+)
+
+$majorMinorVersionNumber = "1.0"
+$thirdVersionPart = "2"
+$basicVersion = $majorMinorVersionNumber + ".0.0"
+$fullVersionNumber = $majorMinorVersionNumber + "." + $thirdVersionPart + "." + $BuildCounter
+
+Write-Host ("##teamcity[buildNumber '" + $fullVersionNumber + "']")
+
+$CommonVersionsCsFile = Get-Item $AssemblyInfoPath
+Write-Output "Updating common version file at $CommonVersionsCsFile"
+$CommonVersionsCsContent = Get-Content $CommonVersionsCsFile -Encoding UTF8
+$AssemblyFileVersionRegex = 'AssemblyFileVersion\(\"(?<p1>\d+)\.(?<p2>\d+)\.(?<p3>\d+)\.(?<p4>\d+)\"'
+$FileVersionReplacement = "AssemblyFileVersion(""$fullVersionNumber"""
+$AssemblyVersionRegex = 'AssemblyVersion\(\"(?<p1>\d+)\.(?<p2>\d+)\.(?<p3>\d+)\.(?<p4>\d+)\"'
+$AssemblyVersionReplacement = "AssemblyVersion(""$basicVersion"""
+($CommonVersionsCsContent -replace $AssemblyFileVersionRegex, $FileVersionReplacement) `
+ -replace $AssemblyVersionRegex, $AssemblyVersionReplacement `
+ | Out-File $CommonVersionsCsFile -Encoding UTF8
+
+
+
+# Set version in .nupkg file
+$nuspecs = Get-ChildItem src -Recurse -Include *.nuspec
+foreach ($nuspectFile in $nuspecs)
+{
+    [xml] $nuspec = Get-Content $nuspectFile
+    $nuspec.package.metadata.version = $fullVersionNumber
+    $nuspec.Save($nuspectFile)
+}
+
+$vsixs =  Get-ChildItem src -Recurse -Include *.vsixmanifest
+foreach ($vsixFile in $vsixs)
+{
+    [xml] $vsixmanifest = Get-Content $vsixFile
+    $vsixmanifest.PackageManifest.Metadata.Identity.Version = $fullVersionNumber
+    $vsixmanifest.Save($vsixFile)
+}
---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Interact Software Ltd.")]
+[assembly: AssemblyProduct("Flyntax")]
+[assembly: AssemblyCopyright("Copyright © 2016 Ian Griffiths")]
+[assembly: AssemblyTrademark("")]
+
+// Note that in automated builds, the AssemblyFileVersion and AssemblyVersion
+// are updated by a script.
+[assembly: AssemblyFileVersion("9999.0.0.0")]
+[assembly: AssemblyVersion("9999.0.0.0")]
