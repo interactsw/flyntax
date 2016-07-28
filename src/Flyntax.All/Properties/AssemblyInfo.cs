e0b01f6a87a54d63c89fe33e49df78ba03407a5f
Ian Griffiths <ian@interact-sw.co.uk>
2016-07-28 21:26:08 +0100

    Add master Flyntax.All project
    Related issue: #12

---
+++ src/Flyntax.All/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Flyntax.all")]
+[assembly: AssemblyDescription("Project used to gather together all analyzers - this DLL is not included, it's just part of the build process")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
